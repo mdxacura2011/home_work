fb84f2a0950a99c2687abbfaa555dceac98c86fc
sergey <sergey.b@magenmagic.com>
2018-11-24 20:23:44 +0200

    task_3_7

--- Training/Test/Plugin/Block/Product/View/Description.php
+++ Training/Test/Plugin/Block/Product/View/Description.php
@@ -5,6 +5,8 @@ class Description
 {
     public function beforeToHtml(\Magento\Catalog\Block\Product\View\Description $subject)
     {
-        $subject->getProduct()->setDescription('Test description');
+        if ($subject->getNameInLayout() == 'product.info.details') {
+                $subject->setTemplate('Training_Test::description.phtml');
+        }
     }
 }
